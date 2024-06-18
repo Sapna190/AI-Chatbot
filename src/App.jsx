82faeb720d5aa5a49ec628c6f0f0932113580364
Sapna190 <sapnasharma8849711@gmail.com>
2024-06-18 17:58:00 +0530

    canged

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,26 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
 
   async function generateAnswer(){
-    console.log("Loading...");
+    setAnswer("Loading...");
     const response = await axios({
       url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBinPBcEPyRSIxghwfG8QNW-rvcZ97KRAc",
       method:"post",
-      data:{contents:[{parts:[{text:"What is an API?"}]}]}
+      data:{contents:[{parts:[{text:question}]}]}
     });
-    console.log(response['data']['candidates'][0]['content']['parts'][0]['text']);
+    setAnswer(response['data']['candidates'][0]['content']['parts'][0]['text']);
   }
-
-
-
-
-
-
-
 
   return (
     <>
       <h1>AI-Chatbot</h1>
+      <textarea value={question} onChange={(e) => setQuestion(e.target.value)} col="100" row="20"></textarea>
       <button onClick={generateAnswer}>Generate answer</button>
+      <pre></pre>
     </>
   )
 }
