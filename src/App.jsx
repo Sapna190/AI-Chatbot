9ec07a7dc87a56c6a67a52187f23a34968e31ae7
Sapna190 <sapnasharma8849711@gmail.com>
2024-06-18 17:32:14 +0530

    checked api working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
-  function generateAnswer(){
-    axios
+  async function generateAnswer(){
+    console.log("Loading...");
+    const response = await axios({
+      url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBinPBcEPyRSIxghwfG8QNW-rvcZ97KRAc",
+      method:"post",
+      data:{contents:[{parts:[{text:"What is an API?"}]}]}
+    });
+    console.log(response['data']['candidates'][0]['content']['parts'][0]['text']);
   }
 
 
@@ -18,6 +25,7 @@ function App() {
   return (
     <>
       <h1>AI-Chatbot</h1>
+      <button onClick={generateAnswer}>Generate answer</button>
     </>
   )
 }
